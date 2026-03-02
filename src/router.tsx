79fd2d020fb064a2867717c1615b3d9cee948c6d
Marlon de Azevedo <marlon.azevedo09@gmail.com>
2026-03-02 20:26:10 +0000

    feat: add and configure shadcn

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
-import HomePage from './pages/shared/home-page.tsx';
-import InstitutionSearch from './pages/institution/institution-search.tsx';
-import InstitutionDetail from './pages/institution/institution-detail.tsx';
-import Login from './pages/auth/login';
-import Register from './pages/auth/register';
-import ParentDashboard from './pages/parent/parent-dashboard.tsx';
-import InstitutionDashboard from './pages/institution/institution-dashboard.tsx';
+import InstitutionSearch from '@/pages/institution/institution-search.tsx';
+import InstitutionDetail from '@/pages/institution/institution-detail.tsx';
+import Login from '@/pages/auth/login';
+import Register from '@/pages/auth/register';
+import InstitutionDashboard from '@/pages/institution/institution-dashboard.tsx';
+import HomePage from "@/pages/shared/home-page.tsx";
+import ParentDashboard from "@/pages/parent/parent-dashboard.tsx";
 
 export const router = createBrowserRouter([
   {
