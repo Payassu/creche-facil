dfff51c1ef55340d70b13e9213f9ccc87c1d7e58
Marlon de Azevedo <marlon.azevedo09@gmail.com>
2026-03-02 23:27:06 +0000

    chore: add biome and restructure routes to share common layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import './index.css'
-import { router } from './router'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import './index.css';
+import { router } from './router';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,8 +13,9 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
-})
+});
 
+// biome-ignore lint/style/noNonNullAssertion: <..>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
@@ -22,4 +23,4 @@ createRoot(document.getElementById('root')!).render(
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
-)
+);
--- src/router.tsx
+++ src/router.tsx
@@ -4,23 +4,46 @@ import InstitutionDetail from '@/pages/institution/institution-detail.tsx';
 import Login from '@/pages/auth/login';
 import Register from '@/pages/auth/register';
 import InstitutionDashboard from '@/pages/institution/institution-dashboard.tsx';
-import HomePage from "@/pages/shared/home-page.tsx";
-import ParentDashboard from "@/pages/parent/parent-dashboard.tsx";
+import HomePage from '@/pages/shared/home-page.tsx';
+import ParentDashboard from '@/pages/parent/parent-dashboard.tsx';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/search',
-    element: <InstitutionSearch />,
-    // loader
-    // errorElement
-  },
-  {
-    path: '/institution/:id',
-    element: <InstitutionDetail />,
+    children: [
+      {
+        element: <HomePage />,
+        index: true,
+      },
+      {
+        path: 'search',
+        element: <InstitutionSearch />,
+        // loader
+        // errorElement
+      },
+      {
+        path: 'parent',
+        children: [
+          {
+            path: 'dashboard',
+            element: <ParentDashboard />,
+          },
+        ],
+      },
+      {
+        path: 'institution',
+        children: [
+          {
+            path: 'dashboard',
+            element: <InstitutionDashboard />,
+          },
+          {
+            path: ':id',
+            element: <InstitutionDetail />,
+          },
+        ],
+      },
+    ],
   },
   {
     path: '/login',
@@ -29,13 +52,5 @@ export const router = createBrowserRouter([
   {
     path: '/register',
     element: <Register />,
-  },
-  {
-    path: '/parent/dashboard',
-    element: <ParentDashboard />,
-  },
-  {
-    path: '/institution/dashboard',
-    element: <InstitutionDashboard />,
   },
 ]);
